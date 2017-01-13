bf00fdb6f645d321055313f151c9e476f6400c93
Jim Padilla <jp_code2013@yahoo.com>
2017-01-12 23:14:59 -0800

    Relate answers to a question

--- app/models/question.js
+++ app/models/question.js
@@ -3,5 +3,6 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   author: DS.attr(),
   title: DS.attr(),
-  note: DS.attr()
+  note: DS.attr(),
+  answerss: DS.hasMany('answer', { async: true })
 });
